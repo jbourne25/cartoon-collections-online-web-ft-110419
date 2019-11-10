1b016de7b611b0a6f73e51d12edc60b130f808bd
Jeremy Bourne <jbourne25@yahoo.com>
2019-11-10 02:45:03 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,7 +1,6 @@
 def roll_call_dwarves(array)
   index = 0  
-  array.each_with_index { |name, index| puts "#{index + 1} "
-    puts #{index} #{name}
+  array.each_with_index { |name, index| puts "#{index + 1}. #{name}.
   end 
 end
 
